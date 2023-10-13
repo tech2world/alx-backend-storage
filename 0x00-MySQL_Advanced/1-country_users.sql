4995cbb827c17cb575c6ab592027b10a2f4aa74b
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-10-13 05:54:06 +0100

    updated 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,7 +3,8 @@
 -- Your script can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );

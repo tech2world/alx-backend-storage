7aca21b3d8b7b88739e70edc83cdc672998ceade
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-10-19 05:36:25 +0100

    updated web.py file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,14 +1,50 @@
 import requests
 import redis
 import time
+from functools import wraps
 
 # Initialize Redis connection
 r = redis.Redis()
 
+def track_count(method):
+    """
+    Decorator to track the number of times a URL was accessed.
+    """
+    @wraps(method)
+    def wrapper(url):
+        """
+        wrapper function
+        """
+        count_key = f"count:{url}"
+        current_count = r.get(count_key)
+        if current_count:
+            r.incr(count_key)
+        else:
+            r.set(count_key, 1)
+        return method(url)
+    return wrapper
+
+def cache_expiring(expiration_time):
+    """
+    Decorator to cache the result of a URL with a specified expiration time.
+    """
+    def decorator(method):
+        @wraps(method)
+        def wrapper(url):
+            cached_result = r.get(f"cache:{url}")
+            if cached_result:
+                return cached_result.decode('utf-8')
+            result = method(url)
+            r.setex(f"cache:{url}", expiration_time, result)
+            return result
+        return wrapper
+    return decorator
+
+@cache_expiring(10)
+@track_count
 def get_page(url: str) -> str:
     """
     Fetches the HTML content of a given URL using the requests module. 
-    Tracks the number of times the URL was accessed and caches the result with an expiration time of 10 seconds.
 
     Args:
         url (str): The URL to fetch the HTML content from.
@@ -16,30 +52,9 @@ def get_page(url: str) -> str:
     Returns:
         str: The HTML content of the given URL.
     """
-    # Check if the URL exists in the cache
-    cached_result = r.get(f"cache:{url}")
-    if cached_result:
-        return cached_result.decode('utf-8')
-
-    # Fetch the content if not present in the cache
     response = requests.get(url)
-    content = response.text
-
-    # Update the cache with the fetched content and set expiration time
-    r.setex(f"cache:{url}", 10, content)
-
-    # Update the count for the URL
-    count_key = f"count:{url}"
-    current_count = r.get(count_key)
-    if current_count:
-        r.incr(count_key)
-    else:
-        r.set(count_key, 1)
-
-    return content
+    return response.text
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.google.com"
-    print(get_page(url))
-    time.sleep(5)  # Simulating the delay for testing expiration
-    print(get_page(url))
+    url = "http://slowwly.robertomurray.co.uk"
+    get_page(url)
